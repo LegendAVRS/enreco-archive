5296245261fc57fb14932d6bb8450629bcebd560
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-11-21 23:06:40 +0700

    chore: ts please

--- src/components/view/LogoSVG.tsx
+++ src/components/view/LogoSVG.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 
-function SvgComponent(props) {
+interface SvgProps {
+    props: React.SVGProps<SVGSVGElement>;
+}
+
+function SvgComponent({ props }: SvgProps) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
