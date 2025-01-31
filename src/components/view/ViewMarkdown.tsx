8e3d1b7ba38010c20f07f97f5ddef0cb72fdf038
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-01-31 15:22:13 +0700

    feat: handling content embeds

--- src/components/view/ViewMarkdown.tsx
+++ src/components/view/ViewMarkdown.tsx
@@ -21,54 +21,89 @@ You can generate these special links by using the following markdown:
 For a link to jump to a specific node: [node link](#node:<node id>)
 For a link to jump to a specific edge: [edge link](#edge:<edge id>)
 */
-export function ViewMarkdown({ onNodeLinkClicked, onEdgeLinkClicked, children }: Props) {
+export function ViewMarkdown({
+    onNodeLinkClicked,
+    onEdgeLinkClicked,
+    children,
+}: Props) {
     const { getNode, getEdge } = useReactFlow<ImageNodeType, CustomEdgeType>();
 
-    const nodeLinkHandler: MouseEventHandler<HTMLAnchorElement> = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        event.preventDefault()
+    const nodeLinkHandler: MouseEventHandler<HTMLAnchorElement> = (
+        event: React.MouseEvent<HTMLAnchorElement>
+    ) => {
+        event.preventDefault();
 
-        const nodeId = (event.target as Element).getAttribute('data-node-id') || '';
+        const nodeId =
+            (event.target as Element).getAttribute("data-node-id") || "";
         const targetNode: ImageNodeType | undefined = getNode(nodeId);
-        if(!targetNode) {
+        if (!targetNode) {
             return;
         }
 
         onNodeLinkClicked(targetNode);
-    }
+    };
 
-    const edgeLinkHandler: MouseEventHandler<HTMLAnchorElement> = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        event.preventDefault()
+    const edgeLinkHandler: MouseEventHandler<HTMLAnchorElement> = (
+        event: React.MouseEvent<HTMLAnchorElement>
+    ) => {
+        event.preventDefault();
 
-        const edgeId = (event.target as Element).getAttribute('data-edge-id') || '';
+        const edgeId =
+            (event.target as Element).getAttribute("data-edge-id") || "";
         const targetEdge: CustomEdgeType | undefined = getEdge(edgeId);
-        if(!targetEdge) {
+        if (!targetEdge) {
             return;
         }
 
         onEdgeLinkClicked(targetEdge);
-    }
+    };
 
     return (
-    <Markdown rehypePlugins={[rehypeRaw]}
-        components={{
-            a(props) {
-                const {href, ...rest} = props;
+        <Markdown
+            rehypePlugins={[rehypeRaw]}
+            components={{
+                a(props) {
+                    const { href, ...rest } = props;
 
-                // Empty href is an easy to retain the correct cursor.
-                if(href && href.startsWith("#node:")) {
-                    const nodeId = href.replace("#node:", "");
-                    return <a href="" data-node-id={nodeId} onClick={nodeLinkHandler} {...rest} />
-                }
-                else if(href && href.startsWith("#edge:")) {
-                    const edgeId = href.replace("#edge:", "");
-                    return <a href="" data-edge-id={edgeId} onClick={edgeLinkHandler} {...rest} />
-                }
-            
-                return <a href={href} target='_blank' {...rest} />
-            }
-        }}
-    >
-        {children}
-    </Markdown>
+                    // Empty href is an easy to retain the correct cursor.
+                    if (href && href.startsWith("#node:")) {
+                        const nodeId = href.replace("#node:", "");
+                        return (
+                            <a
+                                href=""
+                                data-node-id={nodeId}
+                                onClick={nodeLinkHandler}
+                                {...rest}
+                            />
+                        );
+                    } else if (href && href.startsWith("#edge:")) {
+                        const edgeId = href.replace("#edge:", "");
+                        return (
+                            <a
+                                href=""
+                                data-edge-id={edgeId}
+                                onClick={edgeLinkHandler}
+                                {...rest}
+                            />
+                        );
+                    } else if (href && href.startsWith("#embed")) {
+                        const embedUrl = href.replace("#embed:", "");
+                        let caption = rest.children as string;
+                        return (
+                            <figure>
+                                <iframe src={embedUrl} title={caption} />
+                                <figcaption>{caption}</figcaption>
+                            </figure>
+                        );
+                    } else if (href && href.startsWith("#out")) {
+                        return <a href={href} target="_blank" {...rest} />;
+                    }
+
+                    return <a href={href} target="_blank" {...rest} />;
+                },
+            }}
+        >
+            {children}
+        </Markdown>
     );
 }
