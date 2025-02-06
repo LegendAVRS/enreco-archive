9ee0d255b0c89224940c96365f5db137a0c3029c
Walt280 <32001362+Walt280@users.noreply.github.com>
2025-02-06 01:23:20 -0500

    chore: set up new test data

--- src/ViewAppWrapper.tsx
+++ src/ViewAppWrapper.tsx
@@ -1,14 +1,17 @@
 import { Chapter, SiteData } from "./lib/type";
 import ViewApp from "./ViewApp";
 
-import siteMeta from "@/data/site-meta.json";
-import chapter1 from "@/data/chapter0.json";
+import siteMeta from "@/data/test/metadata.json";
+import chapter1 from "@/data/test/chapter0.json";
+import chapter2 from "@/data/test/chapter1.json";
 
 const data: SiteData = {
-    numberOfChapters: siteMeta.numberOfChapters,
-    event: siteMeta.event,
+    version: 1,
+    numberOfChapters: siteMeta.numChapters,
+    event: "Test Data V2",
     chapters: [
-        chapter1 as Chapter
+        chapter1 as Chapter,
+        chapter2 as Chapter
     ]
 }
 
