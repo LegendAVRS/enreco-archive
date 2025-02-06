fc056ec80225559909d03de39b91476071e46391
Walt280 <32001362+Walt280@users.noreply.github.com>
2025-02-05 22:44:43 -0500

    fix: make ViewCustomEdge generate the paths

--- src/components/view/ViewCustomEdge.tsx
+++ src/components/view/ViewCustomEdge.tsx
@@ -1,15 +1,90 @@
 import { EDGE_WIDTH, OLD_EDGE_OPACITY } from "@/lib/constants";
-import { FixedEdgeProps } from "@/lib/type";
+import { generateOrthogonalEdgePath } from "@/lib/custom-edge-svg-path";
+import { CustomEdgeOffsets, FixedEdgeProps } from "@/lib/type";
 import { cn } from "@/lib/utils";
-import { BaseEdge } from "@xyflow/react";
-import { memo } from "react";
+import { BaseEdge, getSmoothStepPath, getStraightPath, Position } from "@xyflow/react";
+import { memo, useMemo } from "react";
 
-const ViewCustomEdge = ({ data, style }: FixedEdgeProps) => {
+function generatePath(
+    pathType: string | undefined, 
+    offsets: CustomEdgeOffsets | undefined, 
+    sourceX: number,
+    sourceY: number,
+    sourcePosition: Position | undefined,
+    targetX: number,
+    targetY: number,
+    targetPosition: Position | undefined
+) {
+    if(pathType === "custom") {
+        if(offsets === undefined) {
+            throw new Error("offsets is undefined!");
+        }
+
+        const path = generateOrthogonalEdgePath(
+            sourceX,
+            sourceY,
+            targetX,
+            targetY,
+            0,
+            offsets
+        );
+
+        return path;
+    }
+    else if(pathType === "smoothstep") {
+        const [path] = getSmoothStepPath({
+            sourceX,
+            sourceY,
+            sourcePosition,
+            targetX,
+            targetY,
+            targetPosition,
+            borderRadius: 0,
+        });
+
+        return path;
+    }
+    else if(pathType === "straight") {
+        const [path] = getStraightPath({
+            sourceX,
+            sourceY,
+            targetX,
+            targetY,
+        });
+
+        return path;
+    }
+    else {
+        throw new Error(`Unkwown pathType ${pathType}`);
+    }
+}
+
+const ViewCustomEdge = ({ 
+    data, 
+    style,
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition
+}: FixedEdgeProps) => {
     const isNew = data?.new || false;
+
+    const path = useMemo(() => generatePath(
+        data?.pathType,
+        data?.offsets,
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition
+    ), [data?.offsets, data?.pathType, sourcePosition, sourceX, sourceY, targetPosition, targetX, targetY]);
 
     return (
         <BaseEdge
-            path={data?.path || ""}
+            path={path}
             className={cn("transition-all", {
                 "pointer-events-none":
                     data?.new === false,
