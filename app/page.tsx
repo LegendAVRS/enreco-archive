d12afcfe465e6af3841c57cdf2bb1b6863f8794e
Walt280 <32001362+Walt280@users.noreply.github.com>
2025-02-06 01:12:18 -0500

    feat: make switch to editor more convenient

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import EditorApp from "@/components/editor/EditorApp";
 import { ViewAppWrapper } from "@/ViewAppWrapper";
 import { ReactFlowProvider } from "@xyflow/react";
 
+const USE_EDITOR = false;
+const inDevEnvironment = !!process && process.env.NODE_ENV === 'development';
+
 const Page = () => {
+    if(USE_EDITOR && inDevEnvironment) {
+        return (
+            <ReactFlowProvider>
+                <EditorApp />
+            </ReactFlowProvider>
+        );
+    }
+    
     return (
         <ReactFlowProvider>
-            {/* <EditorApp /> */}
             <ViewAppWrapper />
         </ReactFlowProvider>
     );
