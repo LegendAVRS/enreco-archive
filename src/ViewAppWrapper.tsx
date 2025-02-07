1c11f377927441ca51a1537813450556670bf0f0
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-02-07 20:21:13 +0700

    data: added day 1

--- src/ViewAppWrapper.tsx
+++ src/ViewAppWrapper.tsx
@@ -1,15 +1,14 @@
 import { Chapter, SiteData } from "./lib/type";
 import ViewApp from "./ViewApp";
 
-import siteMeta from "@/data/test/metadata.json";
-import chapter1 from "@/data/test/chapter0.json";
-import chapter2 from "@/data/test/chapter1.json";
+import siteMeta from "@/data/metadata.json";
+import chapter1 from "@/data/chapter0.json";
 
 const data: SiteData = {
     version: 1,
     numberOfChapters: siteMeta.numChapters,
     event: "Test Data V2",
-    chapters: [chapter1 as Chapter, chapter2 as Chapter],
+    chapters: [chapter1 as Chapter],
 };
 
 export function ViewAppWrapper() {
--- src/components/view/ViewCard.tsx
+++ src/components/view/ViewCard.tsx
@@ -12,12 +12,12 @@ const ViewCard = React.forwardRef<
     return (
         <>
             {isCardOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 z-0 w-screen h-screen md:hidden pointer-events-none" />
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-0 w-screen h-screen lg:hidden pointer-events-none" />
             )}
             <div
                 ref={ref}
                 className={cn(
-                    "right-14 rounded-xl border-2 bg-card text-card-foreground flex flex-col shadow-2xl h-[94%] w-[90%] -translate-x-1/2 left-1/2 md:translate-x-0 top-1/2 -translate-y-1/2 md:left-auto items-center gap-4 p-4 md:w-[40%] z-50",
+                    "right-14 rounded-xl border-2 bg-card text-card-foreground flex flex-col shadow-2xl h-[94%] w-[90%] -translate-x-1/2 left-1/2 md:translate-x-0 top-1/2 -translate-y-1/2 md:left-auto items-center gap-4 p-4 lg:w-[40%] z-50",
                     className,
                 )}
                 {...props}
