7e176a36ca260993483719d742a94b28f4aab9e4
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-02-16 23:28:47 +0700

    style: split visibility toggles using grid

--- src/components/view/ViewVisibilityCard.tsx
+++ src/components/view/ViewVisibilityCard.tsx
@@ -39,127 +39,150 @@ const ViewVisibilityCard = ({
 
     return (
         <div className="flex flex-col gap-4 p-4 overflow-y-auto h-full">
-            <span className="font-bold text-xl">Visibility toggles</span>
-            <div className="flex flex-row justify-between items-center">
-                <Label htmlFor="edge-all">
-                    <span className="font-bold">Edge visibility</span>
-                </Label>
+            <span className="font-bold text-xl">Visibility Toggles</span>
+            <div className="grid md:grid-cols-2 gap-4">
+                {/* Edges */}
+                <div className="flex flex-col gap-4">
+                    <div className="flex justify-between items-center">
+                        <Label htmlFor="edge-all">
+                            <span className="font-bold">
+                                Relationship Toggles
+                            </span>
+                        </Label>
 
-                <Checkbox
-                    id="edge-all"
-                    checked={Object.keys(edgeVisibility).every((key) => {
-                        return key === "new" ? true : edgeVisibility[key];
-                    })}
-                    onCheckedChange={(checked) => {
-                        const newEdgeVisibility = Object.keys(
-                            edgeVisibility,
-                        ).reduce(
-                            (acc, key) => {
-                                if (key === "new") {
-                                    acc[key] = true;
-                                } else {
-                                    acc[key] = checked as boolean;
+                        <Checkbox
+                            id="edge-all"
+                            checked={Object.keys(edgeVisibility).every(
+                                (key) => {
+                                    return key === "new"
+                                        ? true
+                                        : edgeVisibility[key];
+                                },
+                            )}
+                            onCheckedChange={(checked) => {
+                                const newEdgeVisibility = Object.keys(
+                                    edgeVisibility,
+                                ).reduce(
+                                    (acc, key) => {
+                                        if (key === "new") {
+                                            acc[key] = true;
+                                        } else {
+                                            acc[key] = checked as boolean;
+                                        }
+                                        return acc;
+                                    },
+                                    {} as Record<string, boolean>,
+                                );
+                                onEdgeVisibilityChange(newEdgeVisibility);
+                            }}
+                        />
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <Label htmlFor="edge-new">
+                            Show updated edges only
+                        </Label>
+                        <Checkbox
+                            id="edge-new"
+                            checked={edgeVisibility.new}
+                            onCheckedChange={(checked) =>
+                                onEdgeVisibilityChange({
+                                    ...edgeVisibility,
+                                    new: checked as boolean,
+                                })
+                            }
+                        />
+                    </div>
+                    {Object.keys(chapterData.relationships).map((key) => (
+                        <div
+                            className="flex justify-between w-full items-center gap-10"
+                            key={key}
+                        >
+                            <Label htmlFor={`edge-${key.toLowerCase()}`}>
+                                <div className="flex gap-2 items-center">
+                                    {getLineSvg(
+                                        chapterData.relationships[key].style,
+                                    )}
+                                    <span className="capitalize">
+                                        {chapterData.relationships[key].name ||
+                                            key}
+                                    </span>
+                                </div>
+                            </Label>
+
+                            <Checkbox
+                                id={`edge-${key.toLowerCase()}`}
+                                checked={edgeVisibility[key]}
+                                onCheckedChange={(checked) =>
+                                    onEdgeVisibilityChange({
+                                        ...edgeVisibility,
+                                        [key]: checked as boolean,
+                                    })
                                 }
-                                return acc;
-                            },
-                            {} as Record<string, boolean>,
-                        );
-                        onEdgeVisibilityChange(newEdgeVisibility);
-                    }}
-                />
-            </div>
-            <div className="flex flex-row justify-between items-center">
-                <Label htmlFor="edge-new">Show updated edges only</Label>
-                <Checkbox
-                    id="edge-new"
-                    checked={edgeVisibility.new}
-                    onCheckedChange={(checked) =>
-                        onEdgeVisibilityChange({
-                            ...edgeVisibility,
-                            new: checked as boolean,
-                        })
-                    }
-                />
-            </div>
-            {Object.keys(chapterData.relationships).map((key) => (
-                <div
-                    className="flex flex-row justify-between w-full items-center gap-10"
-                    key={key}
-                >
-                    <Label htmlFor={`edge-${key.toLowerCase()}`}>
-                        <div className="flex flex-row gap-2 items-center">
-                            {getLineSvg(chapterData.relationships[key].style)}
-                            <span className="capitalize">
-                                {chapterData.relationships[key].name || key}
-                            </span>
-                        </div>
-                    </Label>
-
-                    <Checkbox
-                        id={`edge-${key.toLowerCase()}`}
-                        checked={edgeVisibility[key]}
-                        onCheckedChange={(checked) =>
-                            onEdgeVisibilityChange({
-                                ...edgeVisibility,
-                                [key]: checked as boolean,
-                            })
-                        }
-                    />
-                </div>
-            ))}
-            <div className="flex flex-row justify-between items-center">
-                <Label htmlFor="team-all">
-                    <span className="font-bold">Team Toggles</span>
-                </Label>
-
-                <Checkbox
-                    id="team-all"
-                    checked={Object.values(teamVisibility).every((v) => v)}
-                    onCheckedChange={(checked) => {
-                        const newTeamVisibility = Object.keys(
-                            teamVisibility,
-                        ).reduce(
-                            (acc, key) => {
-                                acc[key] = checked as boolean;
-                                return acc;
-                            },
-                            {} as Record<string, boolean>,
-                        );
-                        onTeamVisibilityChange(newTeamVisibility);
-                    }}
-                />
-            </div>
-            {Object.keys(chapterData.teams).map((key) => (
-                <div
-                    className="flex flex-row justify-between w-full items-center gap-10"
-                    key={key}
-                >
-                    <Label htmlFor={`team-${key.toLowerCase()}`}>
-                        <div className="flex flex-row gap-2 items-center">
-                            <img
-                                src={chapterData.teams[key].teamIconSrc}
-                                className="w-8 h-8"
-                                alt={`${key} logo`}
                             />
-                            <span className="capitalize">
-                                {chapterData.teams[key].name || key}
-                            </span>
                         </div>
-                    </Label>
-
-                    <Checkbox
-                        id={`team-${key.toLowerCase()}`}
-                        checked={teamVisibility[key]}
-                        onCheckedChange={(checked) =>
-                            onTeamVisibilityChange({
-                                ...teamVisibility,
-                                [key]: checked as boolean,
-                            })
-                        }
-                    />
+                    ))}
                 </div>
-            ))}
-            <div className="flex flex-row justify-between items-center">
+
+                {/* Teams */}
+                <div className="flex flex-col gap-4">
+                    <div className="flex justify-between items-center">
+                        <Label htmlFor="team-all">
+                            <span className="font-bold">Team Toggles</span>
+                        </Label>
+
+                        <Checkbox
+                            id="team-all"
+                            checked={Object.values(teamVisibility).every(
+                                (v) => v,
+                            )}
+                            onCheckedChange={(checked) => {
+                                const newTeamVisibility = Object.keys(
+                                    teamVisibility,
+                                ).reduce(
+                                    (acc, key) => {
+                                        acc[key] = checked as boolean;
+                                        return acc;
+                                    },
+                                    {} as Record<string, boolean>,
+                                );
+                                onTeamVisibilityChange(newTeamVisibility);
+                            }}
+                        />
+                    </div>
+                    {Object.keys(chapterData.teams).map((key) => (
+                        <div
+                            className="flex justify-between w-full items-center gap-10"
+                            key={key}
+                        >
+                            <Label htmlFor={`team-${key.toLowerCase()}`}>
+                                <div className="flex gap-2 items-center">
+                                    <img
+                                        src={chapterData.teams[key].teamIconSrc}
+                                        className="w-8 h-8"
+                                        alt={`${key} logo`}
+                                    />
+                                    <span className="capitalize">
+                                        {chapterData.teams[key].name || key}
+                                    </span>
+                                </div>
+                            </Label>
+
+                            <Checkbox
+                                id={`team-${key.toLowerCase()}`}
+                                checked={teamVisibility[key]}
+                                onCheckedChange={(checked) =>
+                                    onTeamVisibilityChange({
+                                        ...teamVisibility,
+                                        [key]: checked as boolean,
+                                    })
+                                }
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {/* Characters */}
+            <div className="flex justify-between items-center">
                 <Label htmlFor="character-all">
                     <span className="font-bold">Character Toggles</span>
                 </Label>
@@ -181,37 +204,39 @@ const ViewVisibilityCard = ({
                     }}
                 />
             </div>
-            {Object.keys(characterVisibility).map((key) => (
-                <div
-                    className="flex flex-row justify-between w-full items-center gap-10"
-                    key={key}
-                >
-                    <Label htmlFor={`character-${key.toLowerCase()}`}>
-                        <div className="flex flex-row gap-2 items-center">
-                            <img
-                                src={characterImagesMap[key]}
-                                className="w-8 h-8"
-                                alt={`${key} logo`}
-                            />
-                            <span className="capitalize">
-                                {nodes.find((node) => node.id === key)?.data
-                                    .title || key}
-                            </span>
-                        </div>
-                    </Label>
+            <div className="grid md:grid-rows-10 md:grid-cols-2 gap-4">
+                {Object.keys(characterVisibility).map((key) => (
+                    <div
+                        className="flex justify-between w-full items-center gap-10"
+                        key={key}
+                    >
+                        <Label htmlFor={`character-${key.toLowerCase()}`}>
+                            <div className="flex gap-2 items-center">
+                                <img
+                                    src={characterImagesMap[key]}
+                                    className="w-8 h-8"
+                                    alt={`${key} logo`}
+                                />
+                                <span className="capitalize">
+                                    {nodes.find((node) => node.id === key)?.data
+                                        .title || key}
+                                </span>
+                            </div>
+                        </Label>
 
-                    <Checkbox
-                        id={`character-${key.toLowerCase()}`}
-                        checked={characterVisibility[key]}
-                        onCheckedChange={(checked) =>
-                            onCharacterVisibilityChange({
-                                ...characterVisibility,
-                                [key]: checked as boolean,
-                            })
-                        }
-                    />
-                </div>
-            ))}
+                        <Checkbox
+                            id={`character-${key.toLowerCase()}`}
+                            checked={characterVisibility[key]}
+                            onCheckedChange={(checked) =>
+                                onCharacterVisibilityChange({
+                                    ...characterVisibility,
+                                    [key]: checked as boolean,
+                                })
+                            }
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
